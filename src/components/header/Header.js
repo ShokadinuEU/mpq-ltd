fd5516f37a4d90eb3705c22ff1824dcd87ac1b41
Timotei Dumitru <dumitru.timy@gmail.com>
2021-09-19 15:33:22 +0100

    implement MUI and basic version of top menu section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,38 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default function Header() {
-  return <div>This is the header!</div>;
+export default function DenseAppBar() {
+  return (
+    <Box style={{ backgroundColor: "#1a2332" }}>
+      <AppBar position="static" style={{ backgroundColor: "#1a2332" }}>
+        <Toolbar
+          variant="dense"
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{ mr: 2, p: 3 }}
+            fontSize="2em"
+          >
+            MPQ Logo
+          </Typography>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
