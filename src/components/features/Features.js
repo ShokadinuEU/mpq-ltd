a60fbcf78973e3520be590b5b8844b8155f9b846
Timotei Dumitru <dumitru.timy@gmail.com>
2021-09-23 20:05:16 +0100

    add data for exp section

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -4,16 +4,19 @@ import "./features.scss";
 const features = [
   {
     icon: "./assets/images/wide-exp.png",
+    icon1: "./images/wide-exp.png",
     title: "Wide Experience",
     info: "A team of over 10 years experience",
   },
   {
     icon: "./assets/images/accurate.png",
+    icon1: "./images/accurate.png",
     title: "Accurate Estimations",
     info: "Accurate estimations at a competitive prices",
   },
   {
     icon: "./assets/images/100-equip.png",
+    icon1: "./images/100-equip.png",
     title: "100% Equipped",
     info: "With the latest in the protection and safety for all",
   },
@@ -24,11 +27,17 @@ export default function Features() {
     <div className="feature-main">
       {features.map((item) => (
         <div className="feature-individual">
-          <img src={item.icon} alt={item.title} width="70" height="55" />
+          <img src={item.icon1} alt={item.title} width="70" height="55" />
           <h3>{item.title}</h3>
           <p>{item.info}</p>
         </div>
       ))}
+      <img
+        src="images/exp-duct.png"
+        alt="ductwork experience"
+        width="170"
+        height="155"
+      />
     </div>
   );
 }
