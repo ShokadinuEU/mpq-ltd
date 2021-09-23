e0cfce359685a516b4b8b354233f83b8e88da013
Timotei Dumitru <dumitru.timy@gmail.com>
2021-09-23 20:34:42 +0100

    add basic structure and style for info general section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Carousel from "./components/carousel/Carousel";
 import Features from "./components/features/Features";
+import InfoGen from "./components/info-general/InfoGen";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Carousel />
       <Features />
+      <InfoGen />
     </div>
   );
 }
---
+++ src/components/info-general/InfoGen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./info-gen.scss";
+
+export default function InfoGen() {
+  return (
+    <div className="infoGen-main">
+      <h3>
+        MPQ Services operate as specialist ventilation and ductwork contractors
+      </h3>
+      <p>
+        At MPQ Services we operate as specialist ventilation and ductwork
+        contractor. Combining years of experience within the construction
+        industry MPQ Services Limited has the capability and experience to
+        manage a wide range of contracts from modest ductwork fit outs in small
+        shops and residences, to large scale ventilation systems for multi story
+        buildings and commercial dwellings.
+      </p>
+      <p>
+        MPQ Services Limited provide our clients with excellent workmanship and
+        faultless installations, ensuring up to date health and safety standards
+        are adhered to and managed in accordance with the most contemporary
+        construction regulations.
+      </p>
+      <button>Our Services</button>
+    </div>
+  );
+}
