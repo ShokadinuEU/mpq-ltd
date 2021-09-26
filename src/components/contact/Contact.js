4818a1566c1e8507d4db78ef9755c7b00970ff03
Timotei Dumitru <dumitru.timy@gmail.com>
2021-09-26 18:04:47 +0100

    place the brand section at the top of contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,11 +6,34 @@ const companies = [
     name: "Mala",
     img: "./assets/images/mala.png",
   },
+  {
+    name: "Amd",
+    img: "./assets/images/amd.png",
+  },
+  {
+    name: "Bancroft",
+    img: "./assets/images/bancroft.png",
+  },
+  {
+    name: "Farr",
+    img: "./assets/images/farr.png",
+  },
+  {
+    name: "Michel",
+    img: "./assets/images/michel-dale.png",
+  },
 ];
 
 export default function Contact() {
   return (
     <div className="contact-main">
+      <div className="contact-brands">
+        {companies.map((item, i) => (
+          <div key={i}>
+            <img src={item.img} alt={item.name} />{" "}
+          </div>
+        ))}
+      </div>
       <h3>Contact Us</h3>
       <p>
         MPQ Services UK Ltd <br />
@@ -26,7 +49,6 @@ export default function Contact() {
         Mon - Fri: 8:00am - 17:00pm <br />
         Sat & Sun: Closed
       </p>
-      <div className="contact-brands"></div>
     </div>
   );
 }
